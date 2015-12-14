0b9bb3a56c1836239fada7888754d737e594f21a
Stefan <ducdebreme@early-dance.de>
2015-12-14 07:28:53 +0100

    some optimizations on the classes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,8 @@
 
-var $selector = $('.migros-selector');
+var $selector = $('.migros-selector-behavior');
 var name = $selector.attr('name');
 
-var $filter_field = $('<input type="text">').attr('name', name + '-filter');
+var $filter_field = $('<input type="text">').attr('class', name + '-filter');
 var $hidden_field = $('<input type="hidden">').attr('name', name);
 var $new_list = $('<ul class="list-items unstyled"></ul>');
 
@@ -26,17 +26,18 @@ var $wrapper = $('<div class="migros-selector"></div>').append($filter_field, $h
 $selector.replaceWith($wrapper);
 
 // logic for hiding / selecting
-var $item_container = $(".list-items");
-var $selectbox = $(".select-box");
+var $item_container = $(".list-items", $wrapper);
+var $selectbox = $(".group-selector-filter", $wrapper);
 
 $item_container.searcher({
-    inputSelector: ".list-filter",
+    inputSelector: ".group-selector-filter",
     itemSelector: "li",
     textSelector: "a"
 });
 
-$("a", $item_container).click(function(){
+$(".selectable", $item_container).click(function(){
 	var val = $(this).data('value');
+	console.log(val);
 	$selectbox.val(val);
 })
 
